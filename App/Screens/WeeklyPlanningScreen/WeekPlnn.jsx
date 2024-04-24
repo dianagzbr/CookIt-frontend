8ef2f4ee10055514a8ca1bea2fa42b62ce22c212
dianagzbr <alejabribirk4@gmail.com>
2024-04-24 16:26:48 -0600

    TabNavigation

---
+++ App/Screens/WeeklyPlanningScreen/WeekPlnn.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import react from "react";
+
+export default function WeekPlanning() {
+    return (
+        <View>
+            <Text>Planeación Semanal</Text>
+        </View>
+    )
+}
