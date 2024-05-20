1cb62542248ec0db6cd9541b3d6b55e5393a0957
Eber Vladimir <eber.paz9135@alumnos.udg.mx>
2024-05-20 14:13:12 -0600

    logout

--- App/Navigations/DrawerNavigation.jsx
+++ App/Navigations/DrawerNavigation.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -11,113 +15,181 @@ import WeekPlanning from "../Screens/WeeklyPlanningScreen/WeekPlnn";
 import Colors from "../Utils/Colors";
 import MyRecipes from "../Screens/Recipe/MyRecipes";
 import AddRecipe from "../Screens/Recipe/AddRecipe";
+import Login from "../Screens/LoginScreen/Login";
+import Signup from "../Screens/SignupScreen/Signup";
 
 const Drawer = createDrawerNavigator();
 
-const fetchUserData = async (token) =>{}
+const CustomDrawerContent = ({ ...rest }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigation = useNavigation();
 
-const checkAuthentication = async () => {
-  try {
-    const isAuthenticated = await AsyncStorage.getItem('isAuthenticated')
-    return isAuthenticated === 'true'
-  } catch (error) {
-    console.error('Error al obtener el estado de autenticación: ', error);
-    return false
-  }
-}
+  useEffect(() => {
+    getSessionData();
+  }, []);
 
-const CustomDrawerContent = ({  ...rest }) => {
-    const navigation = useNavigation();
+  const getSessionData = async () => {
+    const accessToken = await AsyncStorage.getItem("accessToken");
 
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-    useEffect(() => {
-      const fetchAuthentication = async () => {
-        const authenticated = await checkAuthentication();
-        setIsAuthenticated(authenticated);
-      };
-      fetchAuthentication();
-    },[])
-
-    return (
-      <DrawerContentScrollView {...rest}>
-        <View style={{ alignItems: "center", paddingVertical: 20 }}>
-          <TouchableOpacity onPress={() => navigation.navigate('profile')}>
-            <Image
-              source={require('../../assets/Sadmouse.jpg')}
-              style={{ width: 100, height: 100, borderRadius: 50 }}
-            />
-          </TouchableOpacity>
-          <Text style={{ fontSize: 18, fontWeight: "bold", marginTop: 10 }}>User Name</Text>
-        </View>
-        <DrawerItemList {...rest} />
-      </DrawerContentScrollView>
-    );
+    if (accessToken !== null) {
+      setIsAuthenticated(true);
+    }
   };
 
-const DrawerNavigation = () => {
+  const logout = async () => {
+    await AsyncStorage.removeItem("accessToken")
+    await AsyncStorage.removeItem("refreshToken")
+
+    navigation.navigate("login")
+  }
+
   return (
-    <Drawer.Navigator drawerContent={(props) => <CustomDrawerContent {...props} />}
-        screenOptions={{
+    <DrawerContentScrollView {...rest}>
+      <View style={{ alignItems: "center", paddingVertical: 20 }}>
+        <TouchableOpacity onPress={() => navigation.navigate("profile")}>
+          <Image
+            source={require("../../assets/Sadmouse.jpg")}
+            style={{ width: 100, height: 100, borderRadius: 50 }}
+          />
+        </TouchableOpacity>
+        <Text style={{ fontSize: 18, fontWeight: "bold", marginTop: 10 }}>
+          User Name
+        </Text>
+        {isAuthenticated && (
+          <TouchableOpacity onPress={logout}>
+            <Text>Cerrar seesion</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+      <DrawerItemList {...rest} />
+    </DrawerContentScrollView>
+  );
+};
+
+const DrawerNavigation = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    getSessionData();
+  }, []);
+
+  const getSessionData = async () => {
+    const accessToken = await AsyncStorage.getItem("accessToken");
+
+    if (accessToken !== null) {
+      setIsAuthenticated(true);
+    }
+  };
+
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
         headerStyle: { backgroundColor: Colors.PRIMARY },
-        drawerStyle: { backgroundColor: Colors.PRIMARY, width: '70%', padding: 10 },
-        drawerActiveTintColor: Colors.BLACK
+        drawerStyle: {
+          backgroundColor: Colors.PRIMARY,
+          width: "70%",
+          padding: 10,
+        },
+        drawerActiveTintColor: Colors.BLACK,
       }}
     >
-        <Drawer.Screen 
-            name="Inicio" 
-            component={HomeScreen} 
+      <Drawer.Screen
+        name="Inicio"
+        component={HomeScreen}
+        options={{
+          drawerIcon: ({ focused, size }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={size}
+              color={Colors.BLACK}
+            />
+          ),
+        }}
+      />
+      {isAuthenticated && (
+        <>
+          <Drawer.Screen
+            name="Mis Recetas"
+            component={MyRecipes}
             options={{
-                drawerIcon: ({ focused, size }) => (
-                <Ionicons name={focused ? 'home' : 'home-outline'} size={size} color={Colors.BLACK} />
-                )
-            }} 
-        />
-        <Drawer.Screen 
-            name="Mis Recetas" 
-            component={MyRecipes} 
-            options={{
-                drawerIcon: ({ focused, size }) => (
-                <Ionicons name={focused ? 'restaurant' : 'restaurant-outline'} size={size} color={Colors.BLACK} />
-                )
-            }} 
-        />
-        <Drawer.Screen 
-            name="Favoritos" 
+              drawerIcon: ({ focused, size }) => (
+                <Ionicons
+                  name={focused ? "restaurant" : "restaurant-outline"}
+                  size={size}
+                  color={Colors.BLACK}
+                />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="Favoritos"
             component={Favorites}
             options={{
-                drawerIcon: ({ focused, size }) => (
-                <Ionicons name={focused ? 'heart' : 'heart-outline'} size={size} color={Colors.BLACK} />
-                )
-            }} 
-        />
-        <Drawer.Screen 
-            name="Añadir receta" 
+              drawerIcon: ({ focused, size }) => (
+                <Ionicons
+                  name={focused ? "heart" : "heart-outline"}
+                  size={size}
+                  color={Colors.BLACK}
+                />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="Añadir receta"
             component={AddRecipe}
             options={{
-                drawerIcon: ({ focused, size }) => (
-                <Ionicons name={focused ? 'add-circle' : 'add-circle-outline'} size={size} color={Colors.BLACK} />
-                )
-            }} 
-        />
-        <Drawer.Screen 
-            name="Lista de Compras" 
+              drawerIcon: ({ focused, size }) => (
+                <Ionicons
+                  name={focused ? "add-circle" : "add-circle-outline"}
+                  size={size}
+                  color={Colors.BLACK}
+                />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="Lista de Compras"
             component={ShoppingList}
             options={{
-                drawerIcon: ({ focused, size }) => (
-                <Ionicons name={focused ? 'clipboard' : 'clipboard-outline'} size={size} color={Colors.BLACK} />
-                )
-            }}  
-        />
-        <Drawer.Screen 
-            name="Planeación" 
+              drawerIcon: ({ focused, size }) => (
+                <Ionicons
+                  name={focused ? "clipboard" : "clipboard-outline"}
+                  size={size}
+                  color={Colors.BLACK}
+                />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="Planeación"
             component={WeekPlanning}
             options={{
-                drawerIcon: ({ focused, size }) => (
-                <Ionicons name={focused ? 'calendar' : 'calendar-outline'} size={size} color={Colors.BLACK} />
-                )
-            }} 
-        />
+              drawerIcon: ({ focused, size }) => (
+                <Ionicons
+                  name={focused ? "calendar" : "calendar-outline"}
+                  size={size}
+                  color={Colors.BLACK}
+                />
+              ),
+            }}
+          />
+        </>
+      )}
+      {!isAuthenticated && (
+        <>
+          <Drawer.Screen
+            name="login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <Drawer.Screen
+            name="signup"
+            component={Signup}
+            options={{ headerShown: false }}
+          />
+        </>
+      )}
     </Drawer.Navigator>
   );
 };
