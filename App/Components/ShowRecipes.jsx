90e8fbff0d3176fd6d8f4cd92951cc493f8e17e1
Eber Vladimir <eber.paz9135@alumnos.udg.mx>
2024-05-21 09:50:50 -0600

    create weekly planning

--- App/Components/ShowRecipes.jsx
+++ App/Components/ShowRecipes.jsx
@@ -1,14 +1,22 @@
 import { useEffect, useState } from "react";
-import { Modal, View, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import {
+  Modal,
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 
-const ShowRecipes = ({ children, ...props }) => {
+const ShowRecipes = ({ ...props }) => {
   const [recipes, setRecipes] = useState(null);
 
   const getRecipes = async () => {
     const url = "https://cookit-j5x3.onrender.com/recetas/";
     const response = await fetch(url);
 
-    const data = await response.json()
+    const data = await response.json();
 
     setRecipes(data);
   };
@@ -16,6 +24,11 @@ const ShowRecipes = ({ children, ...props }) => {
   useEffect(() => {
     getRecipes();
   }, []);
+
+  const dataRecipes = (id, name) => {
+    props.setIsModalOpen(false);
+    props.dataRecipes(id, name);
+  };
 
   return (
     <Modal visible={props.isModalOpen} transparent={true} animationType="slide">
@@ -25,11 +38,20 @@ const ShowRecipes = ({ children, ...props }) => {
           <Text>Cargando...</Text>
         ) : (
           <FlatList
+            contentContainerStyle={styles.recipes}
             data={recipes}
             renderItem={({ item }) => (
-              <TouchableOpacity style={styles.button}>
-                <Text>{item.nombre_receta}</Text>
-              </TouchableOpacity>
+              <View style={styles.content}>
+                <TouchableOpacity
+                  onPress={() => dataRecipes(item.id, item.nombre_receta)}
+                >
+                  <Image
+                    style={styles.image}
+                    source={{ uri: item.imagenes[0].imagen }}
+                  />
+                  <Text style={styles.title2}>{item.nombre_receta}</Text>
+                </TouchableOpacity>
+              </View>
             )}
             numColumns={2}
           />
@@ -51,12 +73,27 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 25,
     textAlign: "center",
+    marginBottom: 15,
   },
-  button: {
+  content: {
     backgroundColor: "#EBDCC9",
     padding: 10,
-    borderRadius: 5
-  }
+    borderRadius: 5,
+    flex: 1,
+    width: 150,
+    marginHorizontal: 10,
+  },
+  image: {
+    width: "100%",
+    height: 150,
+    resizeMode: "cover",
+    borderRadius: 5,
+  },
+  title2: {
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 15,
+  },
 });
 
 export default ShowRecipes;
--- App/Screens/WeeklyPlanningScreen/WeekPlnn.jsx
+++ App/Screens/WeeklyPlanningScreen/WeekPlnn.jsx
@@ -1,21 +1,59 @@
 import { View, Text, StyleSheet } from "react-native";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useFocusEffect } from '@react-navigation/native';
 import Button from "../../Components/forms/Button";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function WeekPlanning({ navigation }) {
   const [isWeeklyPlanning, setIsWeekliyPlanning] = useState(false);
+  const [token, setToken] = useState("");
+  const [userData, setUserData] = useState({});
+
+  const getUserToken = async () => {
+    const userToken = await AsyncStorage.getItem("accessToken");
+    const url = "https://cookit-j5x3.onrender.com/auth/users/me/";
+
+    const response = await fetch(url, {
+      headers: {
+        Authorization: "JWT " + userToken,
+      },
+    });
+
+    const data = await response.json();
+
+    setUserData(data);
+    setToken(userToken);
+
+    if (data.planeacion) {
+      setIsWeekliyPlanning(true);
+    }
+  };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      getUserToken();
+    }, [])
+  );
 
   return (
     <View style={styles.container}>
       {!isWeeklyPlanning ? (
         <View style={styles.content}>
           <Text>No hay ninguna planeacion semanal</Text>
-          <Button onPress={() => navigation.navigate("createweek")}>
+          <Button
+            onPress={() =>
+              navigation.navigate("createweek", { token, userData })
+            }
+          >
             Agregar una planeacion
           </Button>
         </View>
       ) : (
-        <Text>Planeación Semanal</Text>
+        <Button
+          onPress={() => navigation.navigate("createweek", { token, userData })}
+        >
+          Eliminar y agregar una nueva
+        </Button>
       )}
     </View>
   );
