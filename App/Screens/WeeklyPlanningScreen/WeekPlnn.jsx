6c0ce90f04070f71671bfd8f720c32ab158c49d9
Eber Vladimir <eber.paz9135@alumnos.udg.mx>
2024-05-21 11:16:49 -0600

    planning design

--- App/Screens/WeeklyPlanningScreen/WeekPlnn.jsx
+++ App/Screens/WeeklyPlanningScreen/WeekPlnn.jsx
@@ -1,8 +1,9 @@
 import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 import React, { useState } from "react";
-import { useFocusEffect } from '@react-navigation/native';
+import { useFocusEffect } from "@react-navigation/native";
 import Button from "../../Components/forms/Button";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function WeekPlanning({ navigation }) {
   const [isWeeklyPlanning, setIsWeekliyPlanning] = useState(false);
@@ -36,12 +37,28 @@ export default function WeekPlanning({ navigation }) {
   );
 
   const renderMeal = (meal) => {
-    if (!meal) return null;
+    if (!meal)
+      return (
+        <View style={styles.containerMeal}>
+          <Text>No hay datos</Text>
+        </View>
+      );
     return (
-      <View>
-        <Text style={styles.mealName}>{meal.nombre_receta}</Text>
-        <Text style={styles.mealDetail}>Calorías: {meal.calorias}</Text>
-        <Text style={styles.mealDetail}>Dificultad: {meal.dificultad}</Text>
+      <View style={styles.containerMeal}>
+        <Image
+          style={styles.imageMeal}
+          source={{ uri: meal.imagenes[0].imagen }}
+        />
+        <View style={styles.mealText}>
+          <Text style={styles.mealName} numberOfLines={1}>
+            {meal.nombre_receta}
+          </Text>
+          <Text style={styles.mealDetail}>Calorías: {meal.calorias}</Text>
+          <Text style={styles.mealDetail}>
+            Tiempo: {meal.tiempo_preparacion}
+          </Text>
+          <Text style={styles.mealDetail}>Dificultad: {meal.dificultad}</Text>
+        </View>
       </View>
     );
   };
@@ -49,23 +66,35 @@ export default function WeekPlanning({ navigation }) {
   const renderDay = ({ item }) => {
     return (
       <View style={styles.dayContainer}>
-        <Text style={styles.dayTitle}>{item.dayName}</Text>
-        <Text>Desayuno:</Text>
-        {renderMeal(item.plan.desayuno)}
-        <Text>Colacion:</Text>
-        {renderMeal(item.plan.colacion)}
-        <Text>Comida:</Text>
-        {renderMeal(item.plan.comida)}
-        <Text>Colacion 2:</Text>
-        {renderMeal(item.plan.colacion2)}
-        <Text>Cena:</Text>
-        {renderMeal(item.plan.cena)}
+        <View style={styles.header}>
+          <Ionicons name="calendar" size={50} color="#B0AFAF" />
+          <Text style={styles.dayTitle}>{item.dayName}</Text>
+        </View>
+        <View style={styles.line} />
+        <Text style={styles.textPlan}>Desayuno:</Text>
+        <View style={styles.line}>{renderMeal(item.plan.desayuno)}</View>
+        <Text style={styles.textPlan}>Colacion:</Text>
+        <View style={styles.line}>{renderMeal(item.plan.colacion)}</View>
+        <Text style={styles.textPlan}>Comida:</Text>
+        <View style={styles.line}>{renderMeal(item.plan.comida)}</View>
+        <Text style={styles.textPlan}>Colacion 2:</Text>
+        <View style={styles.line}>{renderMeal(item.plan.colacion2)}</View>
+        <Text style={styles.textPlan}>Cena:</Text>
+        <View style={styles.line}>{renderMeal(item.plan.cena)}</View>
       </View>
     );
   };
 
   const getPlanningData = () => {
-    const days = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+    const days = [
+      "Lunes",
+      "Martes",
+      "Miércoles",
+      "Jueves",
+      "Viernes",
+      "Sábado",
+      "Domingo",
+    ];
     return days.map((dayName, index) => {
       const dayKey = `dia${index + 1}`;
       return { dayName, plan: userData.planeacion[dayKey] || {} };
@@ -93,7 +122,9 @@ export default function WeekPlanning({ navigation }) {
             keyExtractor={(item) => item.dayName}
           />
           <Button
-            onPress={() => navigation.navigate("createweek", { token, userData })}
+            onPress={() =>
+              navigation.navigate("createweek", { token, userData })
+            }
           >
             Eliminar y agregar una nueva
           </Button>
@@ -110,24 +141,54 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    padding: 10,
   },
   dayContainer: {
     marginVertical: 10,
     padding: 10,
-    backgroundColor: "#fff",
     borderRadius: 5,
   },
   dayTitle: {
-    fontSize: 18,
+    fontSize: 38,
     fontWeight: "bold",
+    marginLeft: 25,
   },
   mealName: {
     fontSize: 16,
     fontWeight: "bold",
+    flex: 1,
   },
   mealDetail: {
     fontSize: 14,
+  },
+  header: {
+    flexDirection: "row",
+  },
+  textPlan: {
+    color: "#858484",
+    fontSize: 20,
+  },
+  line: {
+    marginLeft: 20,
+    marginVertical: 15,
+    borderLeftWidth: 5,
+    borderColor: "#858484",
+    padding: 10,
+  },
+  containerMeal: {
+    backgroundColor: "#F0E8DD",
+    padding: 10,
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  imageMeal: {
+    width: "50%",
+    height: 120,
+    borderRadius: 5,
+  },
+  mealText: {
+    marginLeft: 10,
+    width: "50%",
   },
 });
